8084c8ba376be3d164da73d4313c7b16bde77a06
Neutrovertido <russellcode.james@gmail.com>
2020-03-18 14:02:53 -0600

    JavaScript bases (alpha)

---
+++ js/js_bases.js
@@ -0,0 +1,8 @@
+alert("JavaScript!");
+
+//var name = prompt("Ingresa tu nombre: ");
+var name = "James";
+
+alert("Tu nombre es: "+name);
+document.write("Tu nombre es: "+name);
+console.log("Tu nombre es: "+name);
