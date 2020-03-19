9ac951e379be8f984ae2ac7051788f8675e460dc
Neutrovertido <russellcode.james@gmail.com>
2020-03-19 12:08:29 -0600

    Start of JavaScript personal documentation

---
+++ js/jsdoc.js
@@ -0,0 +1,38 @@
+// JavaScript Documentation by Neutrovertido
+
+// Print values
+// To console (console.log(value); console.log("...${variable}...");)
+// To Document (HTML current file) (document.write(value))
+// Popup window (alert("Value"))
+console.log("Hello!"); // Here we print a String type value that says Hello!
+
+// Ask for values
+// Popup window prompt("Enter some data here:")
+
+// Variables
+/* Types: 
+String('Words'),
+Integer(12),
+Boolean(true/false)
+Float(3.99)
+NULL(null)
+undefined results from using a variable without a value
+NaN(Not a number) results from asking for an operation to something that is not a Integer or Float type
+*/
+// To declare a variable: 
+// var variable = value; var variable; It's used to assign a variable
+// let variable = value; let variable; Same as var;
+// const variable = value; const variable; Assigns a value that cannot be changed later on
+var stringtypevariable = "String";
+let integertypevariable = 12;
+const booleanconstant = false;
+console.log(`A string is ${stringtypevariable}. An integer is ${integertypevariable}. A boolean is ${booleanconstant}`);
+
+/* Operations:
+These operations can be directly printed on
+Sum: +
+Substraction: -
+Multiplication: *
+Division: /
+Residual: %
+*/
